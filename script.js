5418ad2350c82118ae9e9d01c22f2e440d0cb841
jackpouya <jackpouya8@gmail.com>
2025-01-19 21:47:51 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Placeholder JavaScript for interactivity
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", () => {
+        alert("This feature will be available soon!");
+    });
+});
